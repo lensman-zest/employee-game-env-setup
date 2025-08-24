41fba071e264e835ff19377b5875636d0a97b7cf
varshitha-1821 <yogajajula@gmail.com>
2025-08-24 22:22:41 +0530

    EGS-12: Implement basic maze puzzle generation without start/end points

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,1 +1,17 @@
+import unittest
+import time
+from game_engine.timer import GameTimer
+
+class TestGameTimer(unittest.TestCase):
+    def test_timer_starts_and_gets_elapsed_time(self):
+        timer = GameTimer()
+        timer.start()
+        time.sleep(1)
+        self.assertGreater(timer.get_elapsed_time(), 0.9)
+
+    def test_timer_reset(self):
+        timer = GameTimer()
+        timer.start()
+        timer.reset()
+        self.assertEqual(timer.get_elapsed_time(), 0)
  
